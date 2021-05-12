f77b78a6d60e9d1cecb0c2e93be396826ebeeeeb
Abhishek Kumar <iamabhishek229313@gmail.com>
2021-05-12 12:01:32 +0530

    added the upi_screen

--- src/screens/upi_pay_screen.js
+++ src/screens/upi_pay_screen.js
@@ -1,5 +1,5 @@
-import { Ionicons } from '@expo/vector-icons';
-import React from 'react';
+import { Ionicons } from "@expo/vector-icons";
+import React from "react";
 import {
   Dimensions,
   FlatList,
@@ -8,38 +8,40 @@ import {
   Text,
   TextInput,
   View,
-} from 'react-native';
+} from "react-native";
+
+import { AppColors } from "../utils/constants";
 
 const DATA = [
   {
-    id: '1',
-    img: require('../assets/images/amazonPay.png'),
-    title: 'AMAZONPAY',
+    id: "1",
+    img: require("../assets/images/amazonPay.png"),
+    title: "AMAZONPAY",
   },
   {
-    id: '2',
-    img: require('../assets/images/bhim.png'),
-    title: 'BHIM',
+    id: "2",
+    img: require("../assets/images/bhim.png"),
+    title: "BHIM",
   },
   {
-    id: '3',
-    img: require('../assets/images/gpay.png'),
-    title: 'GPAY',
+    id: "3",
+    img: require("../assets/images/gpay.png"),
+    title: "GPAY",
   },
   {
-    id: '4',
-    img: require('../assets/images/paytm.png'),
-    title: 'PAYTM',
+    id: "4",
+    img: require("../assets/images/paytm.png"),
+    title: "PAYTM",
   },
   {
-    id: '5',
-    img: require('../assets/images/phonePe.png'),
-    title: 'PHONEPE',
+    id: "5",
+    img: require("../assets/images/phonePe.png"),
+    title: "PHONEPE",
   },
 ];
 
 export function UPIPayScreen() {
-  const { width, height } = Dimensions.get('window');
+  const { width, height } = Dimensions.get("window");
 
   const renderItem = ({ item }) => (
     <View style={styles.upiItem}>
@@ -51,17 +53,53 @@ export function UPIPayScreen() {
 
   return (
     <View style={styles.container}>
-      <TextInput
-        placeholder="Pay Rs 1.00"
-        keyboardType="numeric"
-        style={styles.textInput}
-      />
-      <Text>Select any UPI option</Text>
-      <View>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "flex-start",
+          alignItems: "baseline",
+          backgroundColor: "#fff",
+          padding: 10,
+          borderRadius: 8,
+          marginBottom: 8,
+          marginHorizontal: 24,
+          marginVertical: 16,
+        }}
+      >
+        <Text style={{ fontWeight: "normal", fontSize: 22 }}>Pay&nbsp;</Text>
+        <Text style={{ fontWeight: "bold", fontSize: 24 }}>₹1.00</Text>
+      </View>
+      <Text style={{ marginHorizontal: 24, marginVertical: 8 }}>
+        Select any UPI option
+      </Text>
+      <View style={{flex:1,backgroundColor: "#fff" }}>
         <FlatList
+          style={{
+            marginTop: 8,
+          }}
           data={DATA}
           renderItem={renderItem}
           keyExtractor={(item) => item.id}
+        />
+      </View>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: 16,
+        }}
+      >
+        <Text style={{ fontSize: 10, color: "rgb(180,180,180)" }}>
+          Powered By
+        </Text>
+
+        <Image
+          resizeMode="contain"
+          source={{
+            uri: "https://s3.ap-south-1.amazonaws.com/cdn.gokwik.co/logo/gokwik-cod-logo.gif",
+          }}
+          style={{ height: height * 0.02, width: width * 0.2 }}
         />
       </View>
     </View>
@@ -71,15 +109,16 @@ export function UPIPayScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 24,
-    backgroundColor: '#fff',
+    paddingBottom: 8,
+    padding: 0,
+    backgroundColor: "rgb(240,240,240)",
   },
   boldText: {
     fontSize: 18,
-    fontWeight: '200',
+    fontWeight: "200",
   },
   textInput: {
-    borderColor: 'black',
+    borderColor: "black",
     borderWidth: 1,
     fontSize: 16,
     padding: 8,
@@ -89,18 +128,19 @@ const styles = StyleSheet.create({
   upiItem: {
     paddingHorizontal: 10,
     paddingVertical: 5,
-    borderWidth: 1,
     borderRadius: 5,
-    marginVertical: 5,
+    marginBottom:8,
     height: 60,
-    flexDirection: 'row',
-    alignItems: 'center',
-
+    flexDirection: "row",
+    alignItems: "center",
+    marginHorizontal: 24,
     elevation: 1,
     shadowOffset: { width: 10, height: 10 },
-    shadowColor: 'rgba(0,0,0,0.5)',
     shadowOpacity: 0.1,
     shadowRadius: 10,
+    backgroundColor: "rgb(253,253,253)",
+    shadowColor: '#fff',
+    elevation:5,
   },
   upiImage: {
     width: 40,
---
+++ src/utils/constants.js
@@ -0,0 +1,8 @@
+// const AppColors {
+//      background: 'rgb(250, 250, 250)',
+//      white : rgb(255, 255, 255),
+//      shade : rgb(252, 252, 252),
+//      greyShade : 'rgb(240, 240, 240)',
+//      blueBorder : 'rgb(3, 101, 214)',
+//      shadow : 'rgb(240, 240, 240)'
+//   };
